2a697727183a9d4f57a90f96c18564d9b7aca009
Dan Ryan <djxr35@gmail.com>
2018-03-16 16:10:35 -0400

    autocomplete form is working

--- GoogleMap.jsx
+++ GoogleMap.jsx
@@ -1,26 +1,106 @@
-import React, {Component} from 'react'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import {createMarker, Marker} from 'google-maps-react'
+import TextField from 'material-ui/TextField';
 
-export class GoogleMap extends Component {
-render() {
-    return (
-      <Map google={this.props.google} zoom={14}>
+export default class GoogleMap extends Component {
 
-        <Marker
-        onClick={this.onMarkerClick}
-        name={'Current location'}
-        />
+  componentDidUpdate() {
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>hi</h1>
-            </div>
-        </InfoWindow>
-      </Map>
-    );
+    this.loadMap(); // call loadMap function to load the google map
+  }
+
+  loadMap() {
+    //var location= {lat: 40.2549, lng:-75.0890}
+    if (this.props && this.props.google) { // checks to make sure that props have been passed
+      const {google} = this.props; // sets props equal to google
+      const maps = google.maps; // sets maps to google maps props
+      console.log(google)
+      const mapRef = this.refs.map; // looks for HTML div ref 'map'. Returned in render below.
+      const node = ReactDOM.findDOMNode(mapRef); // finds the 'map' div in the React DOM, names it node
+      const mapConfig = Object.assign({}, {
+
+        center: google.loc, // sets center of google map to NYC.
+        zoom: 11, // sets zoom. Lower numbers are zoomed further out.
+        mapTypeId: 'roadmap' // optional main map layer. Terrain, satellite, hybrid or roadmap--if unspecified, defaults to roadmap.
+      })
+
+      this.map = new maps.Map(node, mapConfig); // creates a new Google map on the specified node (ref='map') with the specified configuration set above.
+      var service = new google.maps.places.PlacesService(this.map)
+      var request = {
+        location: google.loc,
+        radius: '500',
+        type: ['bar']
+      };
+
+      var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(-33.8902, 151.1759),
+        new google.maps.LatLng(-33.8474, 151.2631));
+
+        var input = document.getElementById('pac-input');
+        var options = {
+          bounds: defaultBounds,
+          types: ['establishment']
+        };
+
+      var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+
+      // var marker = new google.maps.Marker({
+      //   position: google.loc,
+      //   map: this.map,
+      //   title: 'boobs'
+      // })
+      service.nearbySearch(request, (results, status) => {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          //console.log(google.maps.Marker)
+          var resultArr = []
+          for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            //console.log(place.geometry.location.lat(),place.geometry.location.lng())
+            var marker = new google.maps.Marker( {position: {lat:place.geometry.location.lat(),lng: place.geometry.location.lng()}, map: this.map , title: 'work'})
+            resultArr.push(place)
+          }
+          console.log(resultArr)
+          //return resultArr
+        }
+
+        //var marker = new google.maps.Marker( {position: {}, map: this.map , title: 'work'})
+        //console.log(place.geometry.location.lat(),resultArr[0].geometry.location.lng())
+      })
+    }
+  }
+
+  render() {
+    const style = { // MUST specify dimensions of the Google map or it will not work. Also works best when style is specified inside the render function and created as an object
+      width: '90vw', // 90vw basically means take up 90% of the width screen. px also works.
+      height: '75vh' // 75vh similarly will take up roughly 75% of the height of the screen. px also works.
+    }
+
+    return ( // in our return function you must return a div with ref='map' and style.
+     <div>
+      <div ref="map" style={style}>
+        <button/>
+        loading map...
+      </div>
+      <input id='pac-input' class='controls' type = 'text' placeholder ='search for location'/>
+    </div>
+
+    )
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBNO9SHxnyzMG6J1FCDYcle7DjXMjg6jBU")
-})(GoogleMap)
+// var defaultBounds = new this.props.google.maps.LatLngBounds(
+//   new this.props.google.maps.LatLng(-33.8902, 151.1759),
+//   new this.props.google.maps.LatLng(-33.8474, 151.2631));
+
+//   var input = document.getElementById('pac-input');
+//   var options = {
+//     bounds: defaultBounds,
+//     types: ['establishment']
+//   };
+
+// var autocomplete = new this.props.google.maps.places.Autocomplete(input, options);
+
+
+
--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,14 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+module.exports = firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyBw0znJKxpOz2z56yO-gRiNuyTu_iXSusc",
+  "databaseURL": "https://map-it-fsa.firebaseio.com",
+  "storageBucket": "map-it-fsa.appspot.com",
+  "authDomain": "map-it-fsa.firebaseapp.com",
+  "messagingSenderId": "770393365469",
+  "projectId": "map-it-fsa"
+});
+
